795295b89c87a41b6025528c12d1d9cdbe970b5b
Xiang Zhang <soloman817@gmail.com>
2016-08-12 20:05:55 +0800

    nested executor test

--- tests/AleaTKTest/MLFramework.cs
+++ tests/AleaTKTest/MLFramework.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -9,6 +10,7 @@ using AleaTK.ML;
 using NUnit.Framework;
 using static AleaTK.Library;
 using static AleaTK.ML.Library;
+using Executor = AleaTK.ML.Executor;
 
 namespace AleaTKTest
 {
@@ -16,26 +18,65 @@ namespace AleaTKTest
     {
         public class ImperativeLoopOp : Differentiable
         {
+            public int SeqLength { get; }
+            public long Size { get; }
+
             public Variable<float> Input { get; }
             public Variable<float> Weight { get; }
             public Variable<float> Output { get; }
+            public Variable<float> Temp { get; }
 
-            public ImperativeLoopOp(Variable<float> input)
+            public Variable<float> SubInput { get; }
+            public Variable<float> SubWeight { get; }
+            public Variable<float> SubOutput { get; }
+
+            public Symbol SubExecutor { get; } = new Symbol();
+
+            public ImperativeLoopOp(Variable<float> input, int seqLength)
             {
+                SeqLength = seqLength;
+
                 Input = input;
                 var size = input.Shape[1];
+                Size = size;
 
-                Weight = Parameter<float>(Fill(Shape.Create(size, size), 1.0f));
+                Weight = Parameter<float>(Fill(Shape.Create(seqLength, size, size), 1.0f));
 
-                Output = Variable<float>(input.Shape);
+                Output = Variable<float>(PartialShape.Create(size, size));
+
+                Temp = AuxVariable<float>();
 
                 AddInput(Input);
                 AddInput(Weight);
                 AddOutput(Output);
+                AddAuxVar(Temp);
+
+                SubInput = Variable<float>(input.Shape);
+                SubWeight = Variable<float>(input.Shape);
+                SubOutput = Dot(SubInput, SubWeight);
+            }
+
+            public override void Initialize(Executor executor)
+            {
+                var subExecutor = new Executor(executor.Context, SubOutput);
+                executor.Objects[SubExecutor] = subExecutor;
+                subExecutor.Initalize();
+                base.Initialize(executor);
             }
 
             public override void Forward(Executor executor)
             {
+                var subExecutor = (Executor)executor.Objects[SubExecutor];
+                var input = executor.GetTensor(Input);
+                var output = executor.GetTensor(Output, Shape.Create(Size, Size));
+                var temp = executor.GetTensor(Temp, Shape.Create(SeqLength - 1, Size, Size));
+
+                for (var t = 0; t < SeqLength; ++t)
+                {
+                    
+                }
+
+
                 throw new NotImplementedException();
             }
 
