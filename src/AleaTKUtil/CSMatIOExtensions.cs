183e70339edf860a5aa3759f195ef5c7e5441c77
Xiang Zhang <soloman817@gmail.com>
2016-10-10 15:47:08 +0200

    packing ml experiment.

--- src/AleaTKUtil/CSMatIOExtensions.cs
+++ src/AleaTKUtil/CSMatIOExtensions.cs
@@ -1,9 +1,38 @@
 using System;
+using System.CodeDom;
+using System.Collections.Generic;
 using csmatio.io;
 using csmatio.types;
 
 namespace AleaTKUtil
 {
+    public class MatFileData
+    {
+        private readonly List<MLArray> _arrays = new List<MLArray>();
+
+        public void Save(string path, bool compress = true)
+        {
+            new MatFileWriter(path, _arrays, compress);
+        }
+
+        public void Add(string name, float[] array)
+        {
+            var marray = new MLSingle(name, array, 1);
+            _arrays.Add(marray);
+        }
+
+        public void Add(string name, float[,] array)
+        {
+            var m = array.GetLength(0);
+            var n = array.GetLength(1);
+            var marray = new MLSingle(name, new [] {m, n});
+            for (var i = 0; i < m; ++i)
+                for (var j = 0; j < n; ++j)
+                    marray.SetReal(array[i, j], i, j);
+            _arrays.Add(marray);
+        }
+    }
+
     public static class CSMatIOExtensions
     {
         public static float GetSingle(this MatFileReader reader, string name)
@@ -64,11 +93,28 @@ namespace AleaTKUtil
         {
             var marray = reader.GetMLArray(name);
             if (!marray.IsSingle) throw new InvalidCastException("data is not of type float");
-            var n = marray.Size;
+            var m = marray.M;
+            var n = marray.N;
+            var l = m*n;
             var darray = (MLSingle) marray;
-            var data = new float[n];
-            for (var i = 0; i < n; ++i)
-                data[i] = darray.GetReal(i);
+            var data = new float[l];
+            for (var i = 0; i < m; ++i)
+                for (var j = 0; j < n; ++j)
+                    data[i*n + j] = darray.GetReal(i, j);
+            return data;
+        }
+
+        public static float[,] GetSingleArray2D(this MatFileReader reader, string name)
+        {
+            var marray = reader.GetMLArray(name);
+            if (!marray.IsSingle) throw new InvalidCastException("data is not of type float");
+            var m = marray.M;
+            var n = marray.N;
+            var darray = (MLSingle) marray;
+            var data = new float[m, n];
+            for (var i = 0; i < m; ++i)
+                for (var j = 0; j < n; ++j)
+                    data[i, j] = darray.GetReal(i, j);
             return data;
         }
 
@@ -112,11 +158,28 @@ namespace AleaTKUtil
         {
             var marray = reader.GetMLArray(name);
             if (!marray.IsInt32) throw new InvalidCastException("data is not of type Int32");
-            var n = marray.Size;
+            var m = marray.M;
+            var n = marray.N;
+            var l = m * n;
             var darray = (MLInt32)marray;
-            var data = new int[n];
-            for (var i = 0; i < n; ++i)
-                data[i] = darray.GetReal(i);
+            var data = new int[l];
+            for (var i = 0; i < m; ++i)
+                for (var j = 0; j < n; ++j)
+                    data[i * n + j] = darray.GetReal(i, j);
+            return data;
+        }
+
+        public static int[,] GetInt32Array2D(this MatFileReader reader, string name)
+        {
+            var marray = reader.GetMLArray(name);
+            if (!marray.IsInt32) throw new InvalidCastException("data is not of type Int32");
+            var m = marray.M;
+            var n = marray.N;
+            var darray = (MLInt32)marray;
+            var data = new int[m, n];
+            for (var i = 0; i < m; ++i)
+                for (var j = 0; j < n; ++j)
+                    data[i, j] = darray.GetReal(i, j);
             return data;
         }
 
