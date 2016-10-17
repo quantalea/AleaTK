42438352a834175734a7f53993f58342b9d6fb48
Xiang Zhang <soloman817@gmail.com>
2016-10-17 13:08:49 +0200

    for release

--- SolutionInfo.fs
+++ SolutionInfo.fs
@@ -4,11 +4,11 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Alea TK")>]
 [<assembly: AssemblyCompanyAttribute("QuantAlea AG.")>]
 [<assembly: AssemblyCopyrightAttribute("QuantAlea AG. 2016-2016")>]
-[<assembly: AssemblyVersionAttribute("0.9.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.0.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.9.0.3")>]
+[<assembly: AssemblyVersionAttribute("0.9.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.9.0.4")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.9.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.0.3"
-    let [<Literal>] InformationalVersion = "0.9.0.3"
+    let [<Literal>] Version = "0.9.0.4"
+    let [<Literal>] InformationalVersion = "0.9.0.4"
